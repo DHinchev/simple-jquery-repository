d4997b279683e6b86125804073d3d7968978cdf1
Daniel <d.r.hinchev@gmail.com>
2018-09-30 03:02:33 +0300

    Functional slider
    fix ie styling issue and a lot of mobile and auto sliding bugs, improved the code, minified the css again

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,126 @@
 (function () {
     var ClassicSlider = function () {
-        var $elementClassic = $("#classic");
-        var $slider = $("#slider");
-        var $prev = $('#prev');
-        var $next = $('#next'); 
-        var $image = $(".sliderImg");
-        var sliderWidth = 0;
+
+        function getSliderElement() {
+            if($(document).find($('[data-slider]')) && $('[data-slider]').attr('data-slider') == 'true') {
+                var sliderId = $('[data-slider]').attr('id');
+                var slider = $('#'+sliderId);
+            } else {
+                throw new Error("Error: data-slider attribute not set on page");
+            }
+            return slider;
+        }
+
+        function getSliderContainerElement() {
+            if($(document).find($('[data-slider-container]')) && $('[data-slider-container]').attr('data-slider-container') == 'true') {
+                var containerId = $('[data-slider-container]').attr('id');
+                var sliderContainer = $('#'+containerId);
+            } else {
+                throw new Error("Error: data-slider-container attribute not set on page");
+            }
+            return sliderContainer;
+        }
+
+        function getSliderTransitionTime() {
+            if($(document).find($('[data-slider-speed]'))) {
+                var imageTransitionTimeValue = $('[data-slider-speed]').attr('data-slider-speed');
+                imageTransitionTime = imageTransitionTimeValue;
+            } else {
+                imageTransitionTime = 5000;
+            }
+            return imageTransitionTime;
+        }
+
+        function getSliderDurationTime() {
+            if($(document).find($('[data-slider-duration]'))) {
+                var animationDurationValue = $('[data-slider-duration]').attr('data-slider-duration');
+                animationDuration = animationDurationValue;
+            } else {
+                animationDuration = 1000;
+            }
+            return animationDuration;
+        }
+
+        function getSliderMobileMobileTime() {
+            if($(document).find($('[data-slider-mobile-duration]'))) {
+                var animationMobileDurationValue = $('[data-slider-mobile-duration]').attr('data-slider-mobile-duration');
+                animationMobileDuration = animationMobileDurationValue;
+            } else {
+                animationMobileDuration = 5000;
+            }
+            return animationMobileDuration;
+        }
+
+        function getSliderPrevArrowElement() {
+            if($(document).find($('[data-slider-prev]')) && $('[data-slider-prev]').attr('data-slider-prev') == 'prev') {
+                var prevValue = $('[data-slider-prev]').attr('id');
+                var prev = $('#'+prevValue);
+            } else {
+                throw new Error("Error: data-slider-prev attribute not set on page");
+            }
+            return prev;
+        }
+
+        function getSliderNextArrowElement() {
+            if($(document).find($('[data-slider-next]')) && $('[data-slider-next]').attr('data-slider-next') == 'next') {
+                var nextValue = $('[data-slider-next]').attr('id');
+                var next = $('#'+nextValue);
+            } else {
+                throw new Error("Error: data-slider-next attribute not set on page");
+            }
+            return next;
+        }
+
+        var $slider = getSliderElement();
+        var $sliderContainer = getSliderContainerElement();
+        var imageTransitionTime = getSliderTransitionTime();
+        var animationDuration = getSliderDurationTime();
+        var animationDurationMobile = getSliderMobileMobileTime();
+        var $prev = getSliderPrevArrowElement();
+        var $next = getSliderNextArrowElement();
+        var $image = getSliderElement().children();
         var interval = null;
         var intervalMobile = null;
-        var imageTransitionTime = 5000;
         var nextLeftOffset = 0;
         var sliderNext = 0;
         var sliderPrev = 0;
         var currentLeftValue = 0;
-        var imageWidth = 1920;
-        var $sliderContainer = $(slider);
-        var sliderChildren = $sliderContainer.children();
-        var sliderChildrenLength = $sliderContainer.children().length;
-        sliderWidth = imageWidth * sliderChildrenLength;
+        var imageWidth = $sliderContainer.width();
+        var imageWidthMobile = imageWidth;
+        var sliderChildren = $($slider).children();
+        var sliderChildrenLength = $($slider).children().length;
+        var sliderWidth = imageWidth * sliderChildrenLength;
         $slider.width(sliderWidth);
 
-        var initSlider = function (slider) {
+        var initSlider = function () {
             sliderNavigation(sliderChildren);
             bulletsNavigation(sliderChildren);
+            resetImageSize();
         }
 
         function autoSlide() {
             currentLeftValue = $("#slider").offset().left;
             if (currentLeftValue <= (-sliderWidth + imageWidth)) {
                 $slider.css("left", 0);
+
             } else {
                 nextLeftOffset = currentLeftValue - imageWidth;
-                $slider.animate({ "left": + nextLeftOffset + "px" }, 1000);
+                $slider.animate({ "left": + nextLeftOffset + "px" }, {
+                    duration: animationDuration,
+                    start: function () {
+                        disableArrows();
+                    },
+                    complete: function () {
+                        enableArrows();
+                    }
+                });
             }
         };
 
         function sliderNavigation(navigationImages) {
             var sliderNavigation = $('<div></div>');
             sliderNavigation.attr('id', 'slider-navigation');
-            $elementClassic.append(sliderNavigation);
+            $sliderContainer.append(sliderNavigation);
             var sliderNavigationContainer = $('<div></div>');
             sliderNavigationContainer.attr('id', 'slider-navigation-container');
             $(sliderNavigation).append(sliderNavigationContainer);
@@ -54,7 +136,7 @@
         function bulletsNavigation(navigationImages) {
             var sliderBulletsNavigation = $('<div></div>');
             sliderBulletsNavigation.attr('id', 'bullets-navigation');
-            $elementClassic.append(sliderBulletsNavigation);
+            $sliderContainer.append(sliderBulletsNavigation);
             var sliderBulletsContainer = $('<div></div>');
             sliderBulletsContainer.attr('id', 'slider-bullets');
             $(sliderBulletsNavigation).append(sliderBulletsContainer);
@@ -66,44 +148,54 @@
             });
         }
 
-        function disableArrows() {
+        var disableArrows = function () {
             $prev.addClass("active");
             $next.addClass("active");
         }
 
-        function enableArrows() {
+        var enableArrows = function () {
             $prev.removeClass("active");
             $next.removeClass("active");
         }
 
-        function moveOffsetLeft () {
+        function moveOffsetLeft() {
             currentLeftValue = $("#slider").offset().left;
-            disableArrows();
             clearInterval(interval);
             if (currentLeftValue >= 0) {
                 sliderPrev = -sliderWidth + imageWidth;
                 $slider.css("left", sliderPrev + "px");
-                enableArrows();
             } else {
                 sliderPrev = currentLeftValue + imageWidth;
-                enableArrows();
-                $slider.animate({ "left": + sliderPrev + "px" }, 1000, enableArrows);
+                $slider.animate({ "left": + sliderPrev + "px" }, {
+                    duration: animationDuration,
+                    start: function () {
+                        disableArrows();
+                    },
+                    complete: function () {
+                        enableArrows()
+                    }
+                });
                 setInterval(interval, imageTransitionTime);
             }
             interval = setInterval(autoSlide, imageTransitionTime);
         }
 
-        function moveOffsetRight () {
+        function moveOffsetRight() {
             currentLeftValue = $("#slider").offset().left;
-            disableArrows();
             clearInterval(interval);
             if (currentLeftValue == (-sliderWidth + imageWidth)) {
                 $slider.css("left", "0px");
-                enableArrows();
             } else {
                 sliderNext = currentLeftValue - imageWidth;
-                enableArrows();
-                $slider.animate({ "left": + sliderNext + "px" }, 1000, enableArrows);
+                $slider.animate({ "left": + sliderNext + "px" }, {
+                    duration: animationDuration,
+                    start: function () {
+                        disableArrows();
+                    },
+                    complete: function () {
+                        enableArrows()
+                    }
+                });
                 setInterval(interval, imageTransitionTime);
             }
             interval = setInterval(autoSlide, imageTransitionTime);
@@ -111,23 +203,21 @@
 
         function navigationClick(e) {
             if (e.target.className === 'sliderNavigationImage') {
-                disableArrows();
                 clearInterval(interval);
                 var moveSliderTo = $(e.target).data('index');
                 $slider.css("left", -(imageWidth * moveSliderTo) + "px");
-                enableArrows();
                 interval = setInterval(autoSlide, imageTransitionTime);
             }
         }
 
-        $prev.click(function(){moveOffsetLeft()});
-        $next.click(function(){moveOffsetRight()});
-        $elementClassic.click(function (e) {navigationClick(e)});
+        $prev.click(function (e) { e.stopPropagation(); moveOffsetLeft(); });
+        $next.click(function (e) { e.stopPropagation(); moveOffsetRight(); });
+        $sliderContainer.click(function (e) {e.stopPropagation(); navigationClick(e); });
         interval = setInterval(autoSlide, imageTransitionTime);
 
         function resetImageSize() {
             $image.each(function () {
-                $(this).width($(document).width());
+                $(this).width(imageWidthMobile);
             });
         }
 
@@ -137,23 +227,25 @@
             disableArrows();
             var sliding = startClientX = startPixelOffset = pixelOffset = currentSlide = 0;
             slideCount = $('.sliderImg').length;
-
-            $elementClassic.on('mousedown touchstart', mobileSlideStart);
-            $elementClassic.on('mouseup touchend', mobileSlideEnd);
-            $elementClassic.on('mousemove touchmove', mobileSlide);
-            $elementClassic.click(function (e) {navigateMobileImages(e)});
+            $slider.width(slideCount * imageWidthMobile);
+            sliderWidth = slideCount * imageWidthMobile;
+            
+            $sliderContainer.on('mousedown touchstart', mobileSlideStart);
+            $sliderContainer.on('mouseup touchend', mobileSlideEnd);
+            $sliderContainer.on('mousemove touchmove', mobileSlide);
+            $sliderContainer.click(function (e) { navigateMobileImages(e) });
             intervalMobile = setInterval(autoSlideMobile, imageTransitionTime);
 
             function autoSlideMobile() {
                 currentLeftValue = $("#slider").offset().left;
-                if (currentLeftValue <= (-sliderWidth + imageWidth)) {
+                if (currentLeftValue <= (-sliderWidth + imageWidthMobile)) {
                     currentSlide = 0;
-                    $slider.animate({ "left": + 0 + "px" }, 200);
+                    $slider.css("left", "0px");
                     checkBulletsPosition(currentSlide);
                 } else {
-                    currentSlide += 1; 
-                    nextLeftOffset = currentLeftValue - imageWidth;
-                    $slider.animate({ "left": + nextLeftOffset + "px" }, 200);
+                    currentSlide += 1;
+                    nextLeftOffset = currentLeftValue - imageWidthMobile;
+                    $slider.animate({ "left": + nextLeftOffset + "px" }, animationDurationMobile);
                     checkBulletsPosition(currentSlide);
                 }
             };
@@ -164,7 +256,7 @@
                     var moveSliderTo = $(e.target).data('bullet-index');
                     $(e.target).siblings().removeClass('active-bullet');
                     $(e.target).addClass('active-bullet');
-                    $slider.animate({ "left": + -(imageWidth * moveSliderTo) + "px" }, 200);
+                    $slider.animate({ "left": + -(imageWidthMobile * moveSliderTo) + "px" }, animationDurationMobile);
                     interval = setInterval(autoSlideMobile, imageTransitionTime);
                 }
                 currentSlide = $('.active-bullet').data('bullet-index');
@@ -202,7 +294,7 @@
 
             function checkBulletsPosition(position) {
                 var bulletTruePosition = $("[data-bullet-index='" + position + "']");
-                if(position != $('.active-bullet').data('bullet-index')) {
+                if (position != $('.active-bullet').data('bullet-index')) {
                     bulletTruePosition.siblings().removeClass('active-bullet');
                     bulletTruePosition.addClass('active-bullet');
                 }
@@ -214,8 +306,8 @@
                     sliding = 0;
                     currentSlide = pixelOffset < startPixelOffset ? currentSlide + 1 : currentSlide - 1;
                     currentSlide = Math.min(Math.max(currentSlide, 0), slideCount - 1);
-                    pixelOffset = currentSlide * -$elementClassic.width();
-                    $slider.animate({ "left": + pixelOffset + "px" }, 200);
+                    pixelOffset = currentSlide * -$sliderContainer.width();
+                    $slider.animate({ "left": + pixelOffset + "px" }, animationDurationMobile);
                     checkBulletsPosition(currentSlide);
                 }
                 intervalMobile = setInterval(autoSlideMobile, imageTransitionTime);
